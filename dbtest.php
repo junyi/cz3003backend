0ad082ee4285f53039b45d21a0924f6abb9f4a81
Hee Jun Yi <junyi.hjy@gmail.com>
2015-03-31 21:56:35 +0800

    push

--- dbtest.php
+++ dbtest.php
@@ -1,9 +1,9 @@
 <?php
-$dbhost = getenv("OPENSHIFT_EXTMYSQL_DB_HOST");
-$dbport = getenv("OPENSHIFT_EXTMYSQL_DB_PORT");
-$dbuser = getenv("OPENSHIFT_EXTMYSQL_DB_USERNAME");
-$dbpwd = getenv("OPENSHIFT_EXTMYSQL_DB_PASSWORD");
-$dbname = getenv("OPENSHIFT_EXTMYSQL_DB_NAME");
+$dbhost = getenv("OPENSHIFT_MYSQL_DB_HOST");
+$dbport = getenv("OPENSHIFT_MYSQL_DB_PORT");
+$dbuser = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+$dbpwd = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+$dbname = "cz3003master";
 $connection = mysql_connect($dbhost.":".$dbport, $dbuser, $dbpwd);
 if (!$connection) {
   echo "Could not connect to database";
@@ -11,10 +11,10 @@ if (!$connection) {
   echo "Connected to database.<br>";
 }
 $dbconnection = mysql_select_db($dbname);
-$query = "SELECT * from users";
+$query = "SELECT * from Staff";
 $rs = mysql_query($query);
 while ($row = mysql_fetch_assoc($rs)) {
-  echo $row['user_id'] . " " . $row['username'] . "\n";
+  echo $row['staffID'] . " " . $row['name'] . "\n";
 }
 mysql_close();
 ?>
